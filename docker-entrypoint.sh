da7c681bd11f53a8760356bdca704e5384b9a230
Piotr Stawarski <piotr.stawarski@zerodowntime.pl>
2019-03-12 11:08:37 +0100

    real fix ;)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -z ${REDIS_MAXMEMORY:+ok} ]; then
-  export REDIS_MAXMEMORY="$(( $(cat /sys/fs/cgroup/memory/memory.limit_in_bytes) * 10 * 9 ))"
+  export REDIS_MAXMEMORY="$(( $(cat /sys/fs/cgroup/memory/memory.limit_in_bytes) / 10 * 9 ))"
 fi
 
 confd -onetime || exit 2
